6ec61a17651fd3167e04c085a98185c1d011caf1
Víctor <98259577+victor5lm@users.noreply.github.com>
2023-01-16 22:02:35 +0100

    Create heatmaps.R

---
+++ heatmap_files/heatmaps.R
@@ -0,0 +1,5 @@
+# Scripts for the generation of heatmaps shown in the MSc Thesis document
+
+# Heatmap for the 20 most important microbial taxa identified by the best ML model trained after the HPF classification of the study cohort
+
+# Heatmap for the 20 most important microbial taxa identified by the best ML model trained after the HEI classification of the study cohort
