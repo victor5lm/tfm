5cd0a62177582af3a88d02720932f1f9691ec41b
Víctor <98259577+victor5lm@users.noreply.github.com>
2023-01-17 16:15:03 +0100

    Update best_ML_model_study_cohort_HEI.R

--- R_scripts/best_ML_model_study_cohort_HEI.R
+++ R_scripts/best_ML_model_study_cohort_HEI.R
@@ -70,6 +70,9 @@ abunda_clr_pheatmap$Sample <- NULL
 abunda_clr_pheatmap_matrix <- as.matrix(abunda_clr_pheatmap)                                            
                                             
 p2 = ComplexHeatmap::HeatmapAnnotation(Importance = anno_barplot(table_imp_mikropml_rf_for_pheatmap$Overall, gp = gpar(fill=ifelse(table_imp_mikropml_rf_for_pheatmap$`groups_imp$groups_imp`==0,"#073444","#bae9fa"))), height = unit(3, "cm"), HEI_Group=groups_for_pheatmap_genuses$Group,col=list(HEI_Group=c("Good HEI"="#073444","Poor HEI"="#bae9fa")),annotation_name_gp = gpar(fontsize = 10, fontface = "bold"), annotation_legend_param = list(title="", labels_gp = gpar(font=2, fontsize = 12)), border = TRUE, gap = unit(1, "mm"))
+
 p3 = ComplexHeatmap::rowAnnotation(HEI_Group=groups_for_pheatmap$Group,col=list(HEI_Group=c("Good HEI"="#073444","Poor HEI"="#bae9fa")),annotation_name_gp = gpar(fontsize = 10, fontface = "bold"), annotation_legend_param = list(title="", labels_gp = gpar(font=2, fontsize = 12)), border = TRUE, show_legend = FALSE, show_annotation_name = FALSE)
+
 heatmap_pheatmat <- ComplexHeatmap::Heatmap(abunda_clr_pheatmap_matrix, cluster_rows = FALSE, top_annotation = p2, right_annotation = p3, col = rev(RColorBrewer::brewer.pal(name = "RdYlBu", n = 9)), cluster_columns = FALSE, show_row_names = FALSE, heatmap_legend_param = list(title="", legend_height = unit(4,"cm"), labels_gp = gpar(font=2), legend_direction = "vertical"), column_names_gp = grid::gpar(fontsize = 10.5, fontface = "bold.italic"), rect_gp = gpar(col = "#073444", lwd = 1), row_split = groups_for_pheatmap$Group, row_gap = unit(2, "mm"), row_title_gp = gpar(col = c("white","white")), column_split = table_imp_mikropml_rf_for_pheatmap$`groups_imp$groups_imp`, column_title_gp = gpar(col = c("white","white")), column_gap = unit(2, "mm"))
-draw(heatmap_pheatmat,heatmap_legend_side = "left", annotation_legend_side = "bottom")                                            
+
+draw(heatmap_pheatmat,heatmap_legend_side = "left", annotation_legend_side = "bottom")
