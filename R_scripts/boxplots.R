bcac067391cb2b51ddf02603ddf830336c065afc
Víctor <98259577+victor5lm@users.noreply.github.com>
2023-01-18 17:06:29 +0100

    Update boxplots.R

--- R_scripts/boxplots.R
+++ R_scripts/boxplots.R
@@ -7,21 +7,11 @@ ggplot(data = tabla_prueba, aes(x = `HPF group`, y = Abundance, label = SampleID
     geom_boxplot() +
     labs(x = "Group", y = "Abundance\n") +
     facet_wrap(~ Genus, scales = "free") + 
-    stat_summary(fun = "mean", geom = "point", aes(shape = "Mean"), size = 2, color = "gold", show.legend = TRUE) + geom_text_repel(aes(label=SampleID),
-                                                                                                                                    color = ifelse(tabla_prueba$SampleID %in% c("PYM107","PYM202","PYM502","PYM511","PYM310","PYM402","PYM404"), "white", NA),     # text color
-                                                                                                                                    bg.color = ifelse(tabla_prueba$SampleID %in% c("PYM107","PYM202","PYM502","PYM511","PYM310","PYM402","PYM404"), "grey30", NA), # shadow color
-                                                                                                                                    bg.r = ifelse(tabla_prueba$SampleID %in% c("PYM107","PYM202","PYM502","PYM511","PYM310","PYM402","PYM404"), 0.15, NA),          # shadow radius
-                                                                                                                                    max.overlaps = Inf,
-                                                                                                                                    point.size = 8,
-                                                                                                                                    min.segment.length = 0,
-                                                                                                                                    segment.color = ifelse(tabla_prueba$SampleID %in% c("PYM107","PYM202","PYM502","PYM511","PYM310","PYM402","PYM404"), "black", NA)) +
+    stat_summary(fun = "mean", geom = "point", aes(shape = "Mean"), size = 2, color = "gold", show.legend = TRUE) + geom_text_repel(aes(label=SampleID), color = ifelse(tabla_prueba$SampleID %in% c("PYM107","PYM202","PYM502","PYM511","PYM310","PYM402","PYM404"), "white", NA), bg.color = ifelse(tabla_prueba$SampleID %in% c("PYM107","PYM202","PYM502","PYM511","PYM310","PYM402","PYM404"), "grey30", NA), bg.r = ifelse(tabla_prueba$SampleID %in% c("PYM107","PYM202","PYM502","PYM511","PYM310","PYM402","PYM404"), 0.15, NA), max.overlaps = Inf, point.size = 8, min.segment.length = 0, segment.color = ifelse(tabla_prueba$SampleID %in% c("PYM107","PYM202","PYM502","PYM511","PYM310","PYM402","PYM404"), "black", NA)) +
     geom_point(color = ifelse(tabla_prueba$SampleID %in% c("PYM107","PYM202","PYM502","PYM511","PYM310","PYM402","PYM404"), "green", "grey")) +
-    geom_signif(comparisons = list(c("Low HPF consumption", "High HPF consumption")),
-                test = "wilcox.test", map_signif_level = TRUE, textsize = 5, fontface = "bold")+
-    theme(
-        plot.title = element_text(color = "#0099f8", size = 18, face = "bold", hjust = 0.5),
-        plot.subtitle = element_text(face = "bold.italic", hjust = 0.5), strip.text = element_text(face="bold.italic", size=10), axis.title.x = element_text(size=12, face="bold", colour = "black"), axis.title.y = element_text(size=12, face="bold", colour = "black"), axis.text.x = element_text(size=10, face="bold", colour = "black"), axis.text.y = element_text(size=10, face="bold", colour = "black"), legend.title = element_text(size=11, face="bold", colour = "white"), legend.text = element_text(face="bold"), legend.position = "bottom")
+    geom_signif(comparisons = list(c("Low HPF consumption", "High HPF consumption")), test = "wilcox.test", map_signif_level = TRUE, textsize = 5, fontface = "bold")+
+    theme(plot.title = element_text(color = "#0099f8", size = 18, face = "bold", hjust = 0.5), plot.subtitle = element_text(face = "bold.italic", hjust = 0.5), strip.text = element_text(face="bold.italic", size=10), axis.title.x = element_text(size=12, face="bold", colour = "black"), axis.title.y = element_text(size=12, face="bold", colour = "black"), axis.text.x = element_text(size=10, face="bold", colour = "black"), axis.text.y = element_text(size=10, face="bold", colour = "black"), legend.title = element_text(size=11, face="bold", colour = "white"), legend.text = element_text(face="bold"), legend.position = "bottom")
         
-        # Boxplot of some microbial genera whose abundances are significantly different between HEI consumption groups (Study cohort)
+# Boxplot of some microbial genera whose abundances are significantly different between HEI consumption groups (Study cohort)
         
         
